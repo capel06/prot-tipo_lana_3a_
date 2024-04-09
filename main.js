b5417b938e9f82fc0c02494de1aca9ee5d24776b
capel06 <161052097+capel06@users.noreply.github.com>
2024-04-09 20:23:17 -0300

    f

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for(let i=0;i <botoes.length;i++) {
+    botoes[i].onclick = function() {
 
-        for(let j=0;j<botoes.length;j++){
+        for(let j=0;j<botoes.length;j++) { 
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -19,12 +19,12 @@ const tempoObjetivo1 = new Date ("2024-06-05T00:00:00");
 const tempoObjetivo2 = new Date ("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date ("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date ("2025-02-01T00:00:00");
-const tempoObjetivo5 = new Date ("2024-012-16T00:00:00");
+const tempoObjetivo5 = new Date ("2024-12-16T00:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4, tempoObjetivo5];
 
 
-function calculaTempo(tempoObjetivo){
+function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let temnpoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(temnpoFinal/1000);
@@ -35,15 +35,15 @@ function calculaTempo(tempoObjetivo){
     segundos %=60
     minutos %=60
     horas %=24
-    if (temnpoFinal > 0){
+    if (temnpoFinal > 0) {
          return [dias,horas,minutos,segundos];
-    } else{
+    } else {
         return [0,0,0,0];
     }
 
 }
 
-function atualizaCronometro(){
+function atualizaCronometro() {
     for(let i=0; i<contadores.length;i++){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
@@ -51,7 +51,7 @@ function atualizaCronometro(){
         document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
-function comecaCronometro(){
+function comecaCronometro() {
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
 }
